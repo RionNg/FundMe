cb90b020a89060c66a29d04236662903e3d88c76
RionNG <147371006+RionNg@users.noreply.github.com>
2023-11-30 18:59:52 +0800

    FundMe.sol

--- FundMe/FundMe.sol
+++ FundMe/FundMe.sol
@@ -1,9 +1,10 @@
-// SPDX-License-Identifier: MIT
-
 // About this contract:
 // 1. Get funds from users
 // 2. Withdraw funds
 // 3. Set a minimum funding value in USD
+
+
+// SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.22;
 
